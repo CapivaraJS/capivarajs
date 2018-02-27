d008bdf29ad5efd659069475d51206d9c0b9e4e8
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-02-27 14:00:01 -0300

    1.1.0-beta

--- example/list.js
+++ example/list.js
@@ -1,14 +1,14 @@
 capivara.component('my-component', {
     template: `        
-        <h1 cp-if="mostrar == 20">Exemplo</h1>
+        <h1 cp-if="mostrar">Exemplo</h1>
         
         <button cp-click="alterar()">Clique para Mostrar</button>
     `,
     controller: function(scope){
-        scope.mostrar = 10;
+        scope.mostrar = false;
 
         scope.alterar = function(){
-            scope.mostrar = 20;
+            scope.mostrar = !scope.mostrar;
         }
 
     }
