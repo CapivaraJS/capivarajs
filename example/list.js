90a0a65183f5b73b25d5af23dce749f4ecf31c9e
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-03-01 14:13:11 -0300

    merge

--- example/list.js
+++ example/list.js
@@ -54,8 +54,6 @@ capivara.component('my-component', {
 
         <h1 cp-if="$ctrl.numero == 6">If</h1>
         <h1 cp-else-if="$ctrl.numero == 8"> Else IF 8 </h1>
-        <h1 cp-else-if="$ctrl.numero == 9"> Else IF 9 </h1>
-        <h1 cp-else> Else</h1>
         
         <button cp-click="$ctrl.alterar()">Clique para Mostrar</button>
     `,
