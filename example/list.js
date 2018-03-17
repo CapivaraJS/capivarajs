39279cf42fa9f9b650c0e1f11b50b78197d5b75c
jcanabarro <joaovictorcana@gmail.com>
2018-03-17 14:31:27 -0300

    aa

--- example/list.js
+++ example/list.js
@@ -5,14 +5,21 @@ capivara.component('my-component', {
 
         <br/>
 
-        <h1> [[ $ctrl.numberOne + ((10 * $ctrl.numberTwo) / 25) ]] </h1>
+        <button class="btn btn-primary" cp-click="$ctrl.clickando()">Click me!</button>
         
     `,
     controller: function (scope) {
         let $ctrl = this;
-        
+
         $ctrl.numberOne = 70;
         $ctrl.numberTwo = 80;
 
+        $ctrl.clickando = function () {
+            capivara.$emit('meuEvento', this, "123Test");
+        }
+
+        capivara.$on('meuEvento', function (anyParam) {
+            console.log(anyParam)
+        });
     }
 });
