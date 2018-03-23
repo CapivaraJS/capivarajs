c0ae7b74eb02a3ff8adc6c47ad23db7a43c2047e
jcanabarro <joaovictorcana@gmail.com>
2018-03-23 11:03:59 -0300

    working on e2e on cp-style

--- test/e2e/cpStyle/test.js
+++ test/e2e/cpStyle/test.js
@@ -4,20 +4,20 @@ module.exports = {
         browser
             .url('http://localhost:1111/test/e2e/cpStyle/template.html')
             .waitForElementVisible('h1', 10000);
-        browser.assert.cssProperty('h1', 'background', 'green');
+        browser.assert.cssProperty('h1', 'background', 'rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box');
         browser
             .pause(1000)
             .click('button[id="btn1"]')
             .pause(1000);
-        browser.assert.cssProperty('p[id="p1"]', 'background', 'red');
+        browser.assert.cssProperty('p[id="p1"]', 'background', 'rgb(0, 0, 255) none repeat scroll 0% 0% / auto padding-box border-box');
         console.log('Passou teste 1!');
         browser.pause(1000);
-        browser.assert.cssProperty('p[id="p1"]', 'background', 'blue');
+        browser.assert.cssProperty('p[id="p1"]', 'background', 'rgb(0, 0, 255) none repeat scroll 0% 0% / auto padding-box border-box');
         browser
             .pause(1000)
-            .click('button[id="btn2]')
+            .click('button[id="btn2"]')
             .pause(1000);
-        browser.assert.cssProperty('p[id="p1"]', "background", "gray");
+        browser.assert.cssProperty('p[id="p1"]', 'background', 'rgb(128, 128, 128) none repeat scroll 0% 0% / auto padding-box border-box');
         console.log('Passou teste 2!');
         browser
             .pause(3000)
