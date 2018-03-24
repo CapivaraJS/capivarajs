120014aab4f05ab4ce8650439829a45355d41379
jcanabarro <joaovictorcana@gmail.com>
2018-03-24 09:09:36 -0300

    fix some tslint errors

--- src/controller.ts
+++ src/controller.ts
@@ -3,12 +3,11 @@ import { Scope } from './scope/scope';
 
 export class Controller {
 
-    constructor(element?: HTMLElement, callback?){
+    constructor(element?: HTMLElement, callback?) {
         const scope = new Scope(element);
-        if (element && element.parentNode && element.parentNode[Constants.SCOPE_ATTRIBUTE_NAME]){
+        if (element && element.parentNode && element.parentNode[Constants.SCOPE_ATTRIBUTE_NAME]) {
             scope.$parent = element.parentNode[Constants.SCOPE_ATTRIBUTE_NAME];
         }
         callback(scope.getScopeProxy());
     }
-
 }
