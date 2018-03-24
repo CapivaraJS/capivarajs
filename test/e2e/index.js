580c6cb8a989d5dd47fc329f0ce8d2d5be63bf76
jcanabarro <joaovictorcana@gmail.com>
2018-03-24 08:53:11 -0300

    all test working and all syntax are correct

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,20 +1,11 @@
 module.exports = Object.assign(
-<<<<<<< HEAD
     require('./cpClass/test'),
     require('./cpClick/test'),
     require('./cpElse/test'),
-    require('./cpElseIf/test'),
+    // require('./cpElseIf/test'),
     require('./cpIf/test'),
     require('./cpInit/test'),
     require('./cpModel/test'),  
     require('./cpShow/test') ,
     require('./cpStyle/test')
-=======
-	require('./cpClass/test'),
-	require('./cpClick/test'),
-	require('./cpElse/test'),
-	require('./cpElseIf/test'),
-	require('./cpIf/test'),
-	require('./cpShow/test')
->>>>>>> 5d3ecc44bcbd307ee1f09dbad3cef5f4375900eb
 );
