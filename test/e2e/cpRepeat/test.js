80498f1002ebc11016fc08734cac484049186bac
jcanabarro <joaovictorcana@gmail.com>
2018-03-24 15:40:59 -0300

    working on e2e for cp-repeat

---
+++ test/e2e/cpRepeat/test.js
@@ -0,0 +1,27 @@
+module.exports = {
+	'cpRepeat': function (browser) {
+		browser.resizeWindow(1920, 1080);
+		browser
+			.url('http://localhost:1111/test/e2e/cpRepeat/template.html')
+			.waitForElementVisible('button', 10000)
+			.pause(1000)
+			.getText('button', function (result) {
+				if (result.value === 'Add Anna') {
+					browser.click('button')
+						.pause(1000);
+					browser.getText('p', function(result) {
+						if( result.value === 'John'){
+							console.log('Passou no teste');
+							browser
+								.pause(1000)
+								.end();
+						} else {
+							console.log('Falhou no Teste')
+								.waitForElementVisible('div', 10);
+						}
+					});
+				}
+			});
+		browser.end();
+	}
+};
