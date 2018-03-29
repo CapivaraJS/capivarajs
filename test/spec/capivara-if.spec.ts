5728ac01affead7380a8ffaa0d5538451e257419
jcanabarro <joaovictorcana@gmail.com>
2018-03-28 21:34:46 -0300

    adding on cp-if

--- test/spec/capivara-if.spec.ts
+++ test/spec/capivara-if.spec.ts
@@ -4,6 +4,7 @@ import capivara from '../../src/index';
 describe('test of if with fixed object', () => {
     const template = `
         <h1 cp-if="$ctrl.isActive">Show this</h1>
+        <button cp-click="$ctrl.click()">Click me!<button>
     `;
     const element = document.createElement('div');
     element.innerHTML = template;
@@ -11,9 +12,19 @@ describe('test of if with fixed object', () => {
         const $ctrl = this;
         $ctrl.isActive = false;
 
+        $ctrl.click = () => {
+            $ctrl.isActive = !$ctrl.isActive;
+        };
+
         $ctrl.$onInit = () => {
-            it('Expected to add the style', () => {
+            it('Expected none html h1 tag', () => {
                 expect(element.querySelector('h1')).toEqual(null);
+            });
+            setTimeout(function() { element.querySelector('button').click(); }, 1000);
+            it('Expected html h1 tag', () => {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')).not.toEqual(null);
+                }, 3000);
             });
         };
     });
