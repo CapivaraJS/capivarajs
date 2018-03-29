46ada9017af02f485207c2c3956152bac3ecc4d9
jcanabarro <joaovictorcana@gmail.com>
2018-03-29 10:13:53 -0300

    adding cp-show tests

---
+++ test/spec/capivara-show.spec.ts
@@ -0,0 +1,52 @@
+import { } from 'jasmine';
+import capivara from '../../src/index';
+
+describe('test of show with fixed object', () => {
+    const template = `
+        <h1 cp-show="$ctrl.isActive">Show this</h1>
+    `;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    capivara.controller(element, function() {
+        const $ctrl = this;
+        $ctrl.isActive = false;
+
+        $ctrl.$onInit = () => {
+            it("Expected to not find the element", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1').style.display).toEqual('none');
+                    done();
+                });
+            });
+        };
+    });
+});
+
+describe('test of show with dynamic object', () => {
+    const template = `
+        <h1 cp-show="$ctrl.isActive">Show this</h1>
+        <button cp-click="$ctrl.click()">Click me!<button>
+    `;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    capivara.controller(element, function() {
+        const $ctrl = this;
+        $ctrl.isActive = true;
+
+        $ctrl.click = () => {
+            $ctrl.isActive = !$ctrl.isActive;
+        };
+
+        $ctrl.$onInit = () => {
+            setTimeout(function() {
+                element.querySelector('button').click();
+            }, 3000);
+            it("Expected to not find the element", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1').style.display).toEqual('');
+                    done();
+                });
+            }, 5000);
+        };
+    });
+});
