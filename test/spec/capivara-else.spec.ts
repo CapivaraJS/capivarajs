4cda0b299ff0204039f4f049fe57791019eea926
jcanabarro <joaovictorcana@gmail.com>
2018-03-29 14:02:55 -0300

    adding else if spec

--- test/spec/capivara-else.spec.ts
+++ test/spec/capivara-else.spec.ts
@@ -5,7 +5,6 @@ describe('test cpElse hide element', () => {
     const template = `
         <h1 cp-if="$ctrl.isActive">Show this</h1>
         <h2 cp-else>Not show this</h2>
-        <button cp-click="$ctrl.click()">Click me!</button>
     `;
     const element = document.createElement('div');
     element.innerHTML = template;
@@ -24,24 +23,24 @@ describe('test cpElse hide element', () => {
     });
 });
 
-describe('test cpElse show element', () => {
-    const template = `
-        <h1 cp-if="$ctrl.isActive">Show this</h1>
-        <h2 cp-else>Not show this</h2>
-    `;
-    const element = document.createElement('div');
-    element.innerHTML = template;
-    capivara.controller(element, function() {
-        const $ctrl = this;
-        $ctrl.isActive = false;
+// describe('test cpElse show element', () => {
+//     const template = `
+//         <h1 cp-if="$ctrl.isActive">Show this</h1>
+//         <h2 cp-else>Not show this</h2>
+//     `;
+//     const element = document.createElement('div');
+//     element.innerHTML = template;
+//     capivara.controller(element, function() {
+//         const $ctrl = this;
+//         $ctrl.isActive = false;
 
-        $ctrl.$onInit = () => {
-            it("Expected h2 not found", function(done) {
-                setTimeout(function() {
-                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
-                    done();
-                });
-            });
-        };
-    });
-});
+//         $ctrl.$onInit = () => {
+//             it("Expected h2 not found", function(done) {
+//                 setTimeout(function() {
+//                     expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
+//                     done();
+//                 });
+//             });
+//         };
+//     });
+// });
