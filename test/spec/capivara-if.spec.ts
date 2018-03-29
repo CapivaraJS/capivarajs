53f1e865c1fcc3b11975c2e683c4553963859bb6
Mateus Miranda <info.mateusmiranda@gmail.com>
2018-03-29 11:38:13 -0300

    cpIf test

--- test/spec/capivara-if.spec.ts
+++ test/spec/capivara-if.spec.ts
@@ -1,10 +1,10 @@
 import { } from 'jasmine';
 import capivara from '../../src/index';
 
-describe('test of if with fixed object', () => {
+describe('test cpIf hide element', () => {
     const template = `
         <h1 cp-if="$ctrl.isActive">Show this</h1>
-        <button cp-click="$ctrl.click()">Click me!<button>
+        <button cp-click="$ctrl.click()">Click me!</button>
     `;
     const element = document.createElement('div');
     element.innerHTML = template;
@@ -12,12 +12,34 @@ describe('test of if with fixed object', () => {
         const $ctrl = this;
         $ctrl.isActive = false;
 
-        $ctrl.click = () => {
-            $ctrl.isActive = !$ctrl.isActive;
+        $ctrl.$onInit = () => {
+            it("Expected h1 not found", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(true);
+                    done();
+                });
+            });
         };
+    });
+});
+
+describe('test cpIf show element', () => {
+    const template = `
+        <h1 cp-if="$ctrl.isActive">Show this</h1>
+    `;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    capivara.controller(element, function() {
+        const $ctrl = this;
+        $ctrl.isActive = true;
 
         $ctrl.$onInit = () => {
-
+            it("Expected h1 not found", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(false);
+                    done();
+                });
+            });
         };
     });
 });
