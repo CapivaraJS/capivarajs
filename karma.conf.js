7bea9d3b4abfb39546a7c3af7c4bf3c1a54430d8
jcanabarro <joaovictorcana@gmail.com>
2018-03-31 10:07:43 -0300

    adding more

--- karma.conf.js
+++ karma.conf.js
@@ -1,82 +1,82 @@
 module.exports = function (config) {
-    config.set({
+	config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
-
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'karma-typescript', 'es6-shim'],
-
-        // list of files / patterns to load in the browser
-        files: [
-            'src/*.ts',
-            'src/**/*.ts',
-            'test/spec/*spec.ts'
-        ],
-
-        // Configure code coverage reporter
-        coverageReporter: {
-            reporters: [
-                // generates ./coverage/lcov.info
-                {type:'lcovonly', subdir: '.'},
-                // generates ./coverage/coverage-final.json
-                {type:'json', subdir: '.'},
-            ]
-        },
-
-        // list of files to exclude
-        exclude: [],
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-            "**/*.ts": ["karma-typescript"]
-        },
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jasmine', 'karma-typescript', 'es6-shim'],
 
-        karmaTypescriptConfig: {
-            compilerOptions: {
-                target: "ES5",
-                lib: ["ES2015", "DOM"]
-            }
-        },
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'karma-typescript', 'coverage'],
+		// list of files / patterns to load in the browser
+		files: [
+			'src/*.ts',
+			'src/**/*.ts',
+			'test/spec/*spec.ts'
+		],
 
+		// Configure code coverage reporter
+		coverageReporter: {
+			reporters: [
+				// generates ./coverage/lcov.info
+				{type:'lcovonly', subdir: '.'},
+				// generates ./coverage/coverage-final.json
+				{type:'json', subdir: '.'},
+			]
+		},
 
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+		// list of files to exclude
+		exclude: [],
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'**/*.ts': ['karma-typescript']
+		},
+
+		karmaTypescriptConfig: {
+			compilerOptions: {
+				target: 'ES5',
+				lib: ['ES2015', 'DOM']
+			}
+		},
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress', 'karma-typescript', 'coverage'],
 
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
+		// web server port
+		port: 9876,
 
 
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity
-    });
-}
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['PhantomJS'],
+
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	});
+};
   
