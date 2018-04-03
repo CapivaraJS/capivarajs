a69d2014d6b421b341bd0c102644fee527fb798b
Felipe Sabadini Facina <fsabadini@hotmail.com>
2018-04-03 11:35:14 -0300

    correção no observe

--- src/core/observer/index.ts
+++ src/core/observer/index.ts
@@ -4,9 +4,6 @@ import { Util } from './util';
 export namespace Observe {
 
     export function observe(obj, handler) {
-        if (obj.__observer__) {
-            return;
-        }
         let props = Util.keys(obj);
         const propsL = props.length;
         for (let i = 0; i < propsL; i++) {
