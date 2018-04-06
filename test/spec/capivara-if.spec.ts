56301a0d8f7037bc6a0974b450ec1c5fe2aead6c
jcanabarro <joaovictorcana@gmail.com>
2018-04-06 09:24:20 -0300

    modifying cp-if tests

--- test/spec/capivara-if.spec.ts
+++ test/spec/capivara-if.spec.ts
@@ -1,9 +1,10 @@
 import { } from 'jasmine';
 import capivara from '../../src/index';
 
-describe('test cpIf hide element', () => {
+describe('test cpIf with static elements', () => {
     const template = `
-        <h1 cp-if="$ctrl.isActive">Show this</h1>
+        <h1 cp-if="$ctrl.isActive">Not Show this</h1>
+        <h2 cp-if="!$ctrl.isActive">Show this</h1>
     `;
     const element = document.createElement('div');
     element.innerHTML = template;
@@ -12,9 +13,10 @@ describe('test cpIf hide element', () => {
         $ctrl.isActive = false;
 
         $ctrl.$onInit = () => {
-            it("Expected h1 not found", function(done) {
+            it("Expected h1 not found and h2 was found", function(done) {
                 setTimeout(function() {
                     expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(true);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
                     done();
                 });
             });
@@ -22,20 +24,27 @@ describe('test cpIf hide element', () => {
     });
 });
 
-describe('test cpIf show element', () => {
+describe('test cpIf with dynamic elements', () => {
     const template = `
         <h1 cp-if="$ctrl.isActive">Show this</h1>
+        <h2 cp-if="!$ctrl.isActive">Not Show this</h1>
+        <button cp-click="$ctrl.clickMe()">Click me!</button>
     `;
     const element = document.createElement('div');
     element.innerHTML = template;
     capivara.controller(element, function() {
         const $ctrl = this;
         $ctrl.isActive = true;
+        $ctrl.clickMe = function() {
+            $ctrl.isActive = !$ctrl.isActive;
+        };
 
         $ctrl.$onInit = () => {
-            it("Expected h1 not found", function(done) {
+            it("Expected h1 found and h2 not found", function(done) {
                 setTimeout(function() {
-                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(false);
+                    element.querySelector('button').click();
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(true);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
                     done();
                 });
             });
