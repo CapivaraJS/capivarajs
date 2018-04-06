079c4a1929fd5b82796f8bc54a2e69872829d8a8
jcanabarro <joaovictorcana@gmail.com>
2018-04-06 09:43:50 -0300

    adding more tests to cp-if

--- test/spec/capivara-if.spec.ts
+++ test/spec/capivara-if.spec.ts
@@ -24,7 +24,30 @@ describe('test cpIf with static elements', () => {
     });
 });
 
-describe('test cpIf with dynamic elements', () => {
+describe('test cpIf with static elements', () => {
+    const template = `
+        <h1 cp-if="$ctrl.isActive">Not Show this</h1>
+        <h2 cp-if="!$ctrl.isActive">Show this</h1>
+    `;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    capivara.controller(element, function() {
+        const $ctrl = this;
+        $ctrl.isActive = true;
+
+        $ctrl.$onInit = () => {
+            it("Expected h2 not found and h1 was found", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(false);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(true);
+                    done();
+                });
+            });
+        };
+    });
+});
+
+describe('test cpIf with first dynamic elements', () => {
     const template = `
         <h1 cp-if="$ctrl.isActive">Show this</h1>
         <h2 cp-if="!$ctrl.isActive">Not Show this</h1>
@@ -38,15 +61,56 @@ describe('test cpIf with dynamic elements', () => {
         $ctrl.clickMe = function() {
             $ctrl.isActive = !$ctrl.isActive;
         };
-
         $ctrl.$onInit = () => {
             it("Expected h1 found and h2 not found", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(false);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(true);
+                    done();
+                });
+            });
+            it("Expected h2 found and h1 not found", function(done) {
                 setTimeout(function() {
                     element.querySelector('button').click();
                     expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(true);
                     expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
                     done();
+                }, 3000);
+            });
+        };
+    });
+});
+
+describe('test cpIf with second dynamic elements', () => {
+    const template = `
+        <h1 cp-if="$ctrl.isActive">Show this</h1>
+        <h2 cp-if="!$ctrl.isActive">Not Show this</h1>
+        <button cp-click="$ctrl.clickMe()">Click me!</button>
+    `;
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    capivara.controller(element, function() {
+        const $ctrl = this;
+        $ctrl.isActive = false;
+        $ctrl.clickMe = function() {
+            $ctrl.isActive = !$ctrl.isActive;
+        };
+
+        $ctrl.$onInit = () => {
+            it("Expected h2 found and h1 not found", function(done) {
+                setTimeout(function() {
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(true);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(false);
+                    done();
                 });
+            });
+            it("Expected h1 found and h2 not found", function(done) {
+                setTimeout(function() {
+                    element.querySelector('button').click();
+                    expect(element.querySelector('h1')['$$cpDestroyed']).toEqual(false);
+                    expect(element.querySelector('h2')['$$cpDestroyed']).toEqual(true);
+                    done();
+                }, 3000);
             });
         };
     });
