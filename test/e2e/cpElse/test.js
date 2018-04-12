2079dd040e5563de81ee613528f373367564bbab
Caio Micheletto <caio.micheletto@gmail.com>
2018-04-11 23:52:05 -0300

    Improving all tests

--- test/e2e/cpElse/test.js
+++ test/e2e/cpElse/test.js
@@ -1,26 +1,15 @@
 module.exports = {
 	'cpElse': function (browser) {
-		browser.resizeWindow(1920, 1080);
 		browser
+			.resizeWindow(1920, 1080)
 			.url('http://localhost:1111/test/e2e/cpElse/template.html')
 			.waitForElementVisible('button', 10000)
 			.pause(1000)
-			.getText('button', function (result) {
-				if (result.value === 'Click to show!') {
-					browser.click('button')
-						.pause(1000)
-						.getText('h1', function (result) {
-							if (result.value === 'This is an other text.') {
-								browser.pause(1000);
-								console.log('Accepted on the test');
-							} else {
-								browser.pause(1000);
-								console.log('Rejected on the test!!!');
-								browser.waitForElementVisible('div', 10);
-							}
-						});
-				}
-			});
-		browser.end();
+			.assert.containsText('button', 'Click to show!')
+			.click('button')
+			.pause(1000)
+			.assert.containsText('h1', 'This is an other text.')
+			.pause(1000)
+			.end();
 	}
 };
--- test/e2e/cpElseIf/test.js
+++ test/e2e/cpElseIf/test.js
@@ -1,31 +1,18 @@
 module.exports = {
 	'cpElseIf': function (browser) {
-		browser.resizeWindow(1920, 1080);
 		browser
+			.resizeWindow(1920, 1080)
 			.url('http://localhost:1111/test/e2e/cpElseIf/template.html')
 			.waitForElementVisible('button', 10000)
 			.pause(1000)
-			.getText('button', function (result) {
-				if (result.value === 'Click to Sum') {
-					browser.click('button')
-						.pause(1000)
-						.getText('button', function (result2) {
-							if (result2.value === 'Click to Sum')
-								browser.click('button')
-									.pause(1000)
-									.getText('h1', function (result3) {
-										if (result3.value === 'H1 with ELSE IF has the sum equal to 3.') {
-											browser.pause(1000);
-											console.log('Accepted on the test!');
-										} else {
-											browser.pause(1000);
-											console.log('Rejected on the test!!!');
-											browser.waitForElementVisible('div', 10);
-										}
-									});
-						});
-				}
-			});
-		browser.end();
+			.assert.containsText('h1','H1 with ELSE has the sum equal to 1.')
+			.click('button')
+			.pause(1000)
+			.assert.containsText('h1', 'H1 with IF has the sum equal to 2.')
+			.click('button')
+			.pause(1000)
+			.assert.containsText('h1', 'H1 with ELSE IF has the sum equal to 3.')
+			.pause(1000)
+			.end();
 	}
 };
