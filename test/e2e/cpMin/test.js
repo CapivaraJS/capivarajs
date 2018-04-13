f1119e525cf85749f08e2dc206c5675361bdae4e
jcanabarro <joaovictorcana@gmail.com>
2018-04-13 07:47:48 -0300

    all of the e2e tests passing

--- test/e2e/cpMin/test.js
+++ test/e2e/cpMin/test.js
@@ -1,16 +1,18 @@
 module.exports = {
-	'cpMax': function (browser) {
+	'cpMin': function (browser) {
 		browser
 			.resizeWindow(1920, 1080)
 			.url('http://localhost:1111/test/e2e/cpMin/template.html')
 			.waitForElementVisible('input', 10000)
 			.pause(1000)
-			.setValue('input', ' 1')
+			.click('input');
+		for (var i = 0; i < 10; i++) {
+			browser
+				.sendKeys('input', browser.Keys.DOWN_ARROW);
+		}
+		browser
+			.assert.containsText('h1', '0')
 			.pause(1000)
-			.assert.containsText('input', '1')
-			.click('input')
-			.pause(1000)
-			.assert.containsText('input', '0')
 			.end();
 	}
 };
--- test/e2e/cpStep/test.js
+++ test/e2e/cpStep/test.js
@@ -5,12 +5,14 @@ module.exports = {
 			.url('http://localhost:1111/test/e2e/cpStep/template.html')
 			.waitForElementVisible('input', 10000)
 			.pause(1000)
-			.setValue('input', ' 0')
+			.click('input');
+		for (var i = 0; i < 10; i++) {
+			browser
+				.sendKeys('input', browser.Keys.UP_ARROW);
+		}
+		browser
+			.assert.containsText('h1', '30')
 			.pause(1000)
-			.assert.containsText('input', '0')
-			.click('input')
-			.pause(1000)
-			.assert.containsText('input', '3')
 			.end();
 	}
 };
