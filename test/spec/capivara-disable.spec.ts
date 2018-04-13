9f7e5674ed68a80606573b27dadc7162c6d7d9ce
jcanabarro <joaovictorcana@gmail.com>
2018-04-13 11:16:11 -0300

    refactoring spec test

--- test/spec/capivara-disable.spec.ts
+++ test/spec/capivara-disable.spec.ts
@@ -6,6 +6,7 @@ describe('Directive cp-disable', () => {
     it('Should check if disable is truth', () => {
         const template = `<button cp-disable="true" cp-click="$ctrl.addOne()"></button>`;
         const element = document.createElement('div');
+        const btnSelect = element.querySelector('button');
         element.innerHTML = template;
         capivara.controller(element, function() {
             const $ctrl = this;
@@ -18,13 +19,14 @@ describe('Directive cp-disable', () => {
                 $ctrl.clicked += 1;
             };
         });
-        element.querySelector('button').click();
+        btnSelect.click();
         expect(element['$instance'].componentScope.$ctrl.clicked).toEqual(0);
     });
 
     it('Should check if disable is false', () => {
         const template = `<button cp-disable="false" cp-click="$ctrl.addOne()"></button>`;
         const element = document.createElement('div');
+        const btnSelect = element.querySelector('button');
         element.innerHTML = template;
         capivara.controller(element, function() {
             const $ctrl = this;
@@ -37,13 +39,14 @@ describe('Directive cp-disable', () => {
                 $ctrl.clicked += 1;
             };
         });
-        element.querySelector('button').click();
+        btnSelect.click();
         expect(element['$instance'].componentScope.$ctrl.clicked).toEqual(1);
     });
 
     it('Should update disable value changed', () => {
         const template = `<button cp-disable="$ctrl.clicked"></button>`;
         const element = document.createElement('div');
+        const btnSelect = element.querySelector('button');
         element.innerHTML = template;
         capivara.controller(element, function() {
             const $ctrl = this;
@@ -52,51 +55,51 @@ describe('Directive cp-disable', () => {
             };
         });
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disable')).toEqual(false);
+            expect(btnSelect.hasAttribute('disable')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = true;
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(true);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(true);
             element['$instance'].componentScope.$ctrl.clicked = 'false';
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = 'true';
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(true);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(true);
             element['$instance'].componentScope.$ctrl.clicked = null;
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = undefined;
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = {};
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = [];
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = 0;
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = 1;
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(true);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(true);
             element['$instance'].componentScope.$ctrl.clicked = '0';
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(false);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(false);
             element['$instance'].componentScope.$ctrl.clicked = '1';
         }, 0);
         setTimeout(function() {
-            expect(element.querySelector('button').hasAttribute('disabled')).toEqual(true);
+            expect(btnSelect.hasAttribute('disabled')).toEqual(true);
         }, 0);
     });
 });
