f603550b1b6c8ff6633b62cac7d8e224e55f1869
Caio Micheletto <caio.micheletto@gmail.com>
2018-04-16 22:27:04 -0300

    Finishing e2e tests

--- test/e2e/cpFocus/test.js
+++ test/e2e/cpFocus/test.js
@@ -2,6 +2,12 @@ module.exports = {
     'cpFocus': function(browser){
         browser
             .resizeWindow(1920, 1080)
-            .url('http://localhost:1111/test/e2e/cpElseIf/template.html')
+            .url('http://localhost:1111/test/e2e/cpFocus/template.html')
+            .assert.containsText('h1', 'Input not Focused')
+            .pause(1000)
+            .click('input')
+            .assert.containsText('h1', 'Input focused')
+            .pause(500)
+            .end()
     }
 }
